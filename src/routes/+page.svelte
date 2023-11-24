d2a515d2a6e8f2fe8f354fa866adbd1f5e05a32e
MMiikael <mandmets.miikael@gmail.com>
2023-11-24 16:39:03 +0000

    Add Tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
+<h1
+	class="text-red-500 p-8 my-8 rounded-xl text-center opacity-95 max-w-[75%] border-2 text-3xl text-red"
+>
+	Testing
+</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
