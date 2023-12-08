698fed527f24e256c7cb49dfc8419b491c8c253d
MMiikael <mandmets.miikael@gmail.com>
2023-12-08 08:13:32 +0000

    Update main page layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,9 @@
 	import CountrySelect from './CountrySelect.svelte';
 </script>
 
-<div class="flex w-[60%] max-w-[60%] h-[20rem] mt-40 justify-center">
+<div
+	class="flex w-full lg:w-[60%] lg:max-w-[60%] h-[20rem] mt-40 justify-center"
+>
 	<div class="w-1/2 flex">
 		<div class="flex-1 flex flex-col">
 			<CountrySelect />
